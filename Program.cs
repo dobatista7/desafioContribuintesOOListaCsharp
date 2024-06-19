d4051747f7a4ab38d8b387b3fa0ae2ede479acbb
Douglas Batista <dobatista7@gmail.com>
2024-06-18 21:53:19 -0300

    criacao da List ArrayList no programa principal para entrada de dados

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace desafioContribuintesOOListaCsharp
+﻿
+using System.Globalization;
+
+namespace desafioContribuintesOOListaCsharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<TaxPayer> taxPayers = new List<TaxPayer>();
+
+            Console.Write("Quantos contribuintes você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i =0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Digite os dados do {i + 1} o contribuinte: ");
+
+                Console.Write("Renda anual com salário:  ");
+                double  salaryIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Renda anual com prestação de serviço: ");
+                double serviceIncome = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+
+                Console.Write("Renda anual com ganho de capital:  ");
+                double capitalIncome = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                Console.Write("Gastos médicos: ");
+                double heathSpending = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Gastos educacionais:  ");
+                double educationSpending = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                TaxPayer taxPayer = new TaxPayer(salaryIncome, serviceIncome, capitalIncome, heathSpending, educationSpending);
+                taxPayers.Add(taxPayer);
+
+            }
+
         }
     }
 }
