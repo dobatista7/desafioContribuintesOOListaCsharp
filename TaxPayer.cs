5cfe85e6779e7908289a6c6241c006e2ae284752
Douglas Batista <dobatista7@gmail.com>
2024-06-18 21:22:33 -0300

    atributos, construtores, getters e setters

--- TaxPayer.cs
+++ TaxPayer.cs
@@ -8,5 +8,21 @@ namespace desafioContribuintesOOListaCsharp
 {
     internal class TaxPayer
     {
+        public double SalaryIncome { get; private set; }
+        public double ServiceIncome { get; private set; }
+        public double CapitalIncome { get; private set; }
+        public double HealthSpeding {  get; private set; }  
+        public double EducationSpending { get; private set; }
+
+        public TaxPayer(double salaryIncome, double serviceIncome, double capitalIncome, double healthSpeding, double educationSpending)
+        {
+            SalaryIncome = salaryIncome;
+            ServiceIncome = serviceIncome;
+            CapitalIncome = capitalIncome;
+            HealthSpeding = healthSpeding;
+            EducationSpending = educationSpending;
+        }
+
+
     }
 }
